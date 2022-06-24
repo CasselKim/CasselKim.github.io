b6168d1cb9cacec0dca8e2cf4c8daab133f97d4d
CasselKim <seongr39@gmail.com>
2022-06-24 14:33:29 +0900

    implement layouts

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path')
 const app = new express();
 const ejs = require('ejs')
-app.set('view sample','ejs');
+app.set('view engine','ejs');
 
 app.use(express.static('public'))
 
-app.get('/',function(req,res){
+app.get('/',(req,res)=>{
     res.render('index');
 })
 
-app.get('/recent',function(req,res){
+app.get('/recent',(req,res)=>{
     res.render('recent');
 })
 
