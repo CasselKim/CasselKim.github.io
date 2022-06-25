0acbdd1e09aeb4588428227003bb237762d0d627
CasselKim <seongr39@gmail.com>
2022-06-25 23:30:03 +0900

    update files

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const path = require('path')
 const app = new express();
 const ejs = require('ejs')
+const http = require('http');
+const querystring = require('querystring');
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+
+(async () => {
+  const client = new DynamoDB({ region: "ap-northeast-2" });
+  try {
+    const results = await client.listTables({});
+    console.log(results.TableNames.join("\n"));
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
 app.set('view engine','ejs');
 
 app.use(express.static('public'))
