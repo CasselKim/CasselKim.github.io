a30cc7f174d48b22cee7cf3a8bb6c911b4184d69
CasselKim <seongr39@gmail.com>
2022-06-26 22:26:57 +0900

    update index and packages

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ app.get('/recent',(req,res)=>{
     res.render('recent');
 })
 
-
-app.listen(4000, ()=>{
-    console.log("App listening on port 4000...")
+let port = process.env.PORT;
+if (port==null || port==""){
+    port=4000;
+}
+app.listen(port, ()=>{
+    console.log('App listening...')
 })
